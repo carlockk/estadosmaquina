9bb30c4fee19b6c245251305c9999de69a59d3cb
carlockk <carlos.virtualdesk@gmail.com>
2025-07-25 00:47:32 -0400

    probando..........

--- app/dashboard/crear/page.jsx
+++ app/dashboard/crear/page.jsx
@@ -4,16 +4,22 @@ import { Suspense } from 'react';
 import CrearMaquina from '../../components/CrearMaquina';
 import { Box, Typography, Divider } from '@mui/material';
 
-export default function CrearElementoPage() {
+function CrearElementoView() {
   return (
     <Box sx={{ width: '100%', mt: 3 }}>
       <Typography variant="h5" sx={{ mb: 2 }}>
         Crear Nuevo Elemento
       </Typography>
       <Divider sx={{ mb: 2 }} />
-      <Suspense fallback={<div>Cargando...</div>}>
-        <CrearMaquina />
-      </Suspense>
+      <CrearMaquina />
     </Box>
+  );
+}
+
+export default function CrearElementoPage() {
+  return (
+    <Suspense fallback={<div>Cargando...</div>}>
+      <CrearElementoView />
+    </Suspense>
   );
 }
