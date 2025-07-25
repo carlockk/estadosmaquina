14863b43d551b124e8da86f338f1afc13e4cbceb
carlockk <carlos.virtualdesk@gmail.com>
2025-07-25 00:10:25 -0400

    funcionando subida de imagen, menu dinamico por categorias, tablas con datos segun se elijan las categorias2

--- app/dashboard/crear/page.jsx
+++ app/dashboard/crear/page.jsx
@@ -1,16 +1,19 @@
 'use client';
 
+import { Suspense } from 'react';
 import CrearMaquina from '../../components/CrearMaquina';
 import { Box, Typography, Divider } from '@mui/material';
 
 export default function CrearElementoPage() {
   return (
-    <Box sx={{ width: '100%', maxWidth: 800, mx: 'auto', mt: 4, px: 2 }}>
+    <Box sx={{ width: '100%', mt: 3 }}>
       <Typography variant="h5" sx={{ mb: 2 }}>
         Crear Nuevo Elemento
       </Typography>
-      <Divider sx={{ mb: 3 }} />
-      <CrearMaquina />
+      <Divider sx={{ mb: 2 }} />
+      <Suspense fallback={<div>Cargando...</div>}>
+        <CrearMaquina />
+      </Suspense>
     </Box>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Suspense } from 'react';
 import CrearMaquina from './components/CrearMaquina';
 import LayoutDashboard from './components/LayoutDashboard';
 import { Box, Typography, Divider } from '@mui/material';
@@ -7,13 +8,15 @@ import { Box, Typography, Divider } from '@mui/material';
 export default function Home() {
   return (
     <LayoutDashboard>
-      <Box sx={{ width: '100%', maxWidth: 800, mx: 'auto', mt: 4, px: 2 }}>
-        <Typography variant="h5" sx={{ mb: 2 }}>
-          Crear Nuevo Elemento
-        </Typography>
-        <Divider sx={{ mb: 3 }} />
-        <CrearMaquina />
-      </Box>
+      <Suspense fallback={<div>Cargando...</div>}>
+        <Box sx={{ width: '100%', maxWidth: 800, mx: 'auto', mt: 4, px: 2 }}>
+          <Typography variant="h5" sx={{ mb: 2 }}>
+            Crear Nuevo Elemento
+          </Typography>
+          <Divider sx={{ mb: 3 }} />
+          <CrearMaquina />
+        </Box>
+      </Suspense>
     </LayoutDashboard>
   );
 }
