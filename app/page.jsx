a2e21a2e0ed933a6f2ea45148b5227b9c3beccec
carlockk <carlos.virtualdesk@gmail.com>
2025-07-25 00:22:17 -0400

    funcionando subida de imagen, menu dinamico por categorias, tablas con datos segun se elijan las categorias4

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+// app/page.jsx
 'use client';
 
 import { Suspense } from 'react';
@@ -5,17 +6,23 @@ import CrearMaquina from './components/CrearMaquina';
 import LayoutDashboard from './components/LayoutDashboard';
 import { Box, Typography, Divider } from '@mui/material';
 
+function CrearElementoView() {
+  return (
+    <Box sx={{ width: '100%', maxWidth: 800, mx: 'auto', mt: 4, px: 2 }}>
+      <Typography variant="h5" sx={{ mb: 2 }}>
+        Crear Nuevo Elemento
+      </Typography>
+      <Divider sx={{ mb: 3 }} />
+      <CrearMaquina />
+    </Box>
+  );
+}
+
 export default function Home() {
   return (
     <LayoutDashboard>
       <Suspense fallback={<div>Cargando...</div>}>
-        <Box sx={{ width: '100%', maxWidth: 800, mx: 'auto', mt: 4, px: 2 }}>
-          <Typography variant="h5" sx={{ mb: 2 }}>
-            Crear Nuevo Elemento
-          </Typography>
-          <Divider sx={{ mb: 3 }} />
-          <CrearMaquina />
-        </Box>
+        <CrearElementoView />
       </Suspense>
     </LayoutDashboard>
   );
